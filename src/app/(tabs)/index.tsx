5395ddec4b81576b1ddf430d36b59a577d4873e1
samueloluwadoromi <samueloluwadoromi@gmail.com>
2024-04-17 17:15:39 -0500

    TrackListItem created

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,56 +4,29 @@ import {
   Dimensions,
   SafeAreaView,
   Image,
+  FlatList,
 } from 'react-native';
 
 import EditScreenInfo from '@/src/components/EditScreenInfo';
 import { Text, View, } from '@/src/components/Themed';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { tracks } from '@/assets/data/tracks';
+import TrackListItem from '@/src/components/TrackListItem';
 
 const {width, height} = Dimensions.get('window');
 
-export default function TabOneScreen() {
+export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <View style={styles.mainContainer}>
-        <View style={styles.shadowed}>
-          <View style={styles.musicControls}>
-              <TouchableOpacity onPress={() => {}}>
-                  <Ionicons name="shuffle" size={30} color="#EDF4F2" />
-              </TouchableOpacity>
-
-              <TouchableOpacity onPress={() => {}}>
-                  <Ionicons name="play-skip-back" size={30} color="#EDF4F2" />
-              </TouchableOpacity>
-
-              <TouchableOpacity onPress={() => {}}>
-                  <Ionicons
-                      name={'play-circle'}
-                      size={75}
-                      color="#EDF4F2"
-                  />
-              </TouchableOpacity>
-
-              <TouchableOpacity onPress={() => {}}>
-                  <Ionicons name="play-skip-forward" size={30} color="#EDF4F2" />
-              </TouchableOpacity>
-
-              <TouchableOpacity onPress={() => {}}>
-                  <Ionicons name="repeat" size={30} color="#EDF4F2" />
-              </TouchableOpacity>
-          </View>
-          <View style={styles.bottomContainer}>
-            <View style={styles.bottomIconWrapper}>
-              <TouchableOpacity>
-                <Ionicons name="share-outline" size={25} color="#EDF4F2" />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => {}}>
-                  <Ionicons name="heart-outline" size={25} color="#EDF4F2" />
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
-      </View>
+      
+      <FlatList 
+        data={tracks}
+        renderItem={( {item} ) => 
+          <TrackListItem tracks={ item }/>
+        }
+        showsVerticalScrollIndicator={false}
+      />
+      
     </View>
   );
 }
@@ -61,45 +34,7 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#31473A',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-  mainContainer: {
-    flex: 1,
-    fontFamily: 'Helvetica',
-  },
-  shadowed: {
-    alignItems: 'center',
-    paddingTop: 20,
-    backgroundColor: 'rgba(0, 0, 0, 0.9)',
-    //backgroundColor: 'rgba(237, 244, 242, 0.5)',
-  },
-  musicControls: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '65%',
-    marginBottom: 15,
-  },
-  bottomContainer: {
-    width: width,
-    alignItems: 'center',
-    paddingVertical: 15,
-  },
-  bottomIconWrapper: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '85%',
-  },
-});
+  });
---
+++ src/components/TrackListItem.tsx
@@ -0,0 +1,58 @@
+import { Text, View, StyleSheet, Image, Dimensions } from 'react-native';
+import { Track } from '../types';
+
+type TrackListItemProps = {
+    tracks: Track
+}
+
+const {width, height} = Dimensions.get('window');
+
+export default function TrackListItem({ tracks }: TrackListItemProps){
+    return(
+        <View style={styles.container}>
+            <Image source={{ uri: tracks.album.images[0]?.url}} style={styles.image}/>
+
+            <View style={styles.trackDetails}>
+                <Text style={styles.title}>
+                    {tracks.name}
+                </Text>
+                <Text style={styles.subtitle}>
+                    {tracks.artists[0]?.name}
+                </Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        //backgroundColor: '#31473A',
+        marginVertical: 5,
+        padding: 5,
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: width,
+    },
+    trackDetails:{
+        width: width - 85,
+    },
+    title: {
+        color: '#EDF4F2',
+        fontWeight: '600',
+        fontSize: 14,
+        marginBottom: 1,
+    },
+    subtitle: {
+        //color: '#31473A',
+        color: '#EDF4F2',
+        fontWeight: '300',
+        fontSize: 12,
+        marginTop: 1,
+    },
+    image: {
+        width: 64, 
+        aspectRatio: 1,
+        borderRadius: 5, 
+        marginRight: 10,
+    },
+})
